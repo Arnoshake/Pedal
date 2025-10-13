8ad51769446a27ff71cd5dbf8019a4bb345e0074
arnoshake-admin <zwest2563@gmail.com>
2025-10-13 13:29:47 -0500

    finding output given x and h using convolution

--- main.py
+++ main.py
@@ -3,24 +3,6 @@ import matplotlib.pyplot as plt
 from scipy.signal import chirp
 from scipy import signal
 
-t = np.linspace(0,10,10000) # start: 0, stop: 10, increments: 1000x --> sampling interval = (10-0)/(1000-1) = 0.01001 seconds
-# sampling rate = samples/second = 1 / 0.01001 = 99.9Hz
-
-y = chirp(t,f0=6,f1=1,t1=10,method="linear")
-    # chirp is applied across the linspace of t, the frequency spans 6Hz to 1Hz
-    # it goes from 0 until t1
-    # the chirp is linear
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(t,y)
-
-ax.set_title('Chirp Signal')
-ax.set_xlabel('Time (t)')
-ax.set_ylabel('Amplitude')
-ax.grid(True)
-
-# Display the plot
 
 
 def time_to_index(time, t_array): #GPT helper function to convert a time to an index for np.arrays
@@ -45,22 +27,65 @@ def simulate_chirp_response(linSpace, delay,gain,signal):
     # THis function is the first step to eventually assuming/determining h(t) from a given x(t) and y(t)
 
     # assumptions: delay and gain are same length
+def simulate_chirp_response_convolution(input_linSpace, delay,gain,signal):
+    #assign the gain value of index to the spot of the delay
+    h = np.zeros( time_to_index(delay[-1],input_linSpace) + 1)
+    for index,val in enumerate(delay):
+        x = time_to_index(delay[index],input_linSpace)
+        y = gain[index]
+        h[x] = y
 
-x = np.array([1,2,3,4,5])
-d = np.array([0,0.003,0.010]) #passing in time in terms of seconds!
-g = np.array([1,-0.7,0.5])
-o =simulate_chirp_response(t,d,g,x)
+    # need to recreate h by combining the data from delay and gain
+    output_signal = np.convolve(signal, h)
+    return output_signal
+def practice_functions(t):
+    x = np.array([1,2,3,4,5])
+    d = np.array([0,0.003,0.010]) #passing in time in terms of seconds!
+    g = np.array([1,-0.7,0.5])
+    o =simulate_chirp_response(t,d,g,x)
+    o2 = simulate_chirp_response_convolution(t,d,g,x)
 
-print (o)
-extended_t = np.linspace(0, len(o)/len(t)*t[-1], len(o))
-plt.figure(figsize=(10,4))
-plt.plot(extended_t[:50], o[:50], marker='o', linestyle='-', markersize=4)  # first 50 samples
-plt.title("Zoomed-In Chirp Response")
-plt.xlabel("Time (s)")
-plt.ylabel("Amplitude")
-plt.grid(True)
-plt.show()
+    extended_t = np.linspace(0, len(o)/len(t)*t[-1], len(o))
+    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
 
+    axs[0].plot(extended_t[:50], o[:50], marker='o', linestyle='-', markersize=4)
+    axs[0].set_title("Zoomed-In Chirp Response")
+    axs[0].set_xlabel("Time (s)")
+    axs[0].set_ylabel("Amplitude")
+    axs[0].grid(True)
+
+    axs[1].plot(extended_t[:len(o2)], o2, marker='o', linestyle='-', markersize=4)
+    axs[1].set_title("Zoomed-In Chirp Response Convolution")
+    axs[1].set_xlabel("Time (s)")
+    axs[1].set_ylabel("Amplitude")
+    axs[1].grid(True)
+
+    plt.tight_layout()  # makes sure labels/titles don’t overlap
+    plt.show()
+
+def main():
+    t = np.linspace(0,10,10000) # start: 0, stop: 10, increments: 1000x --> sampling interval = (10-0)/(1000-1) = 0.01001 seconds
+    # sampling rate = samples/second = 1 / 0.01001 = 99.9Hz
+
+    y = chirp(t,f0=6,f1=1,t1=10,method="linear")
+        # chirp is applied across the linspace of t, the frequency spans 6Hz to 1Hz
+        # it goes from 0 until t1
+        # the chirp is linear
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(t,y)
+
+    ax.set_title('Chirp Signal')
+    ax.set_xlabel('Time (t)')
+    ax.set_ylabel('Amplitude')
+    ax.grid(True)
+
+    practice_functions(t)
+
+print("SOP")
+main()
+print("EOP")
 
 
 
