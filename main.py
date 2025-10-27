fb9a5e3ea98e18123f77a79e30647978ce4bb079
arnoshake-admin <zwest2563@gmail.com>
2025-10-27 14:36:11 -0500

    creating spectrogram. Putting it in organized plotlib graph

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 from scipy.signal import chirp
 from scipy import signal
 
-
-
 def time_to_index(time, t_array): #GPT helper function to convert a time to an index for np.arrays
     dt = t_array[1] - t_array[0]
     return int(time / dt)
@@ -38,7 +36,7 @@ def simulate_chirp_response_convolution(input_linSpace, delay,gain,signal):
     # need to recreate h by combining the data from delay and gain
     output_signal = np.convolve(signal, h)
     return output_signal
-def practice_functions(t):
+def practice_functions(chirp_signal,t):
     x = np.array([1,2,3,4,5])
     d = np.array([0,0.003,0.010]) #passing in time in terms of seconds!
     g = np.array([1,-0.7,0.5])
@@ -46,46 +44,69 @@ def practice_functions(t):
     o2 = simulate_chirp_response_convolution(t,d,g,x)
 
     extended_t = np.linspace(0, len(o)/len(t)*t[-1], len(o))
-    fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+    fig, axs = plt.subplots(1, 4, figsize=(12, 4))
 
-    axs[0].plot(extended_t[:50], o[:50], marker='o', linestyle='-', markersize=4)
-    axs[0].set_title("Zoomed-In Chirp Response")
-    axs[0].set_xlabel("Time (s)")
-    axs[0].set_ylabel("Amplitude")
+    axs[0].plot(t,chirp_signal)
+    axs[0].set_title('Chirp Signal')
+    axs[0].set_xlabel('Time (t)')
+    axs[0].set_ylabel('Amplitude')
     axs[0].grid(True)
 
-    axs[1].plot(extended_t[:len(o2)], o2, marker='o', linestyle='-', markersize=4)
-    axs[1].set_title("Zoomed-In Chirp Response Convolution")
+    axs[1].plot(extended_t[:50], o[:50], marker='o', linestyle='-', markersize=4)
+    axs[1].set_title("Zoomed-In Chirp Response")
     axs[1].set_xlabel("Time (s)")
     axs[1].set_ylabel("Amplitude")
     axs[1].grid(True)
 
+    axs[2].plot(extended_t[:len(o2)], o2, marker='o', linestyle='-', markersize=4)
+    axs[2].set_title("Zoomed-In Chirp Response Convolution")
+    axs[2].set_xlabel("Time (s)")
+    axs[2].set_ylabel("Amplitude")
+    axs[2].grid(True)
+    
+
     plt.tight_layout()  # makes sure labels/titles don’t overlap
-    plt.show()
+    return fig,axs[3]
+def create_spectrogram(sampling_freq, data, ax=None):
+    # Create spectrogram on the provided axes or a new one
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10, 5))
+    Pxx, freqs, bins, im = ax.specgram(data, Fs=sampling_freq, cmap="rainbow")
+    
+    ax.set_title('Spectrogram')
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Frequency [Hz]')
+    
+    return ax  # return the axes object
 
 def main():
-    t = np.linspace(0,10,10000) # start: 0, stop: 10, increments: 1000x --> sampling interval = (10-0)/(1000-1) = 0.01001 seconds
+    START = 0
+    STOP = 10
+    NUM_SAMPLES = 10000
+    t = np.linspace(START,STOP,NUM_SAMPLES) # start: 0, stop: 10, increments: 1000x --> sampling interval = (10-0)/(1000-1) = 0.01001 seconds
+    
+    SAMPLING_INTERVAL = (STOP - START)/(NUM_SAMPLES - 1)
+    SAMPLING_FREQUENCY = 1/ SAMPLING_INTERVAL
     # sampling rate = samples/second = 1 / 0.01001 = 99.9Hz
 
-    y = chirp(t,f0=6,f1=1,t1=10,method="linear")
+    NYQUIST_FREQUENCY = SAMPLING_FREQUENCY / 2
+    
+    chirp_end_freq = 1000 #WHAT USER WANTS 
+    max_chirp_frequency = min(chirp_end_freq,NYQUIST_FREQUENCY) #ensures aliasing does not occur
+    y = chirp(t,f0=0,f1=max_chirp_frequency,t1=10,method="linear") # your max frequency of chirp can only be 1/2 of your sampling frequency --> NYQUIST THEOREM
         # chirp is applied across the linspace of t, the frequency spans 6Hz to 1Hz
         # it goes from 0 until t1
-        # the chirp is linear
+  
+    results, ax3 = practice_functions(y,t)
+    spectro_fig = create_spectrogram(SAMPLING_FREQUENCY,y,ax3)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(t,y)
-
-    ax.set_title('Chirp Signal')
-    ax.set_xlabel('Time (t)')
-    ax.set_ylabel('Amplitude')
-    ax.grid(True)
-
-    practice_functions(t)
+    plt.figure(results)
+    plt.show()
 
 print("SOP")
 main()
 print("EOP")
+
 
 
 
